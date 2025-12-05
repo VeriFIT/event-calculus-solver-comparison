c2797b300c3c76e575ece684d765eb8853e60ba1
ivasicek <ivasicek@fit.vut.cz>
2025-12-05 10:42:50 +0100

    lpx utils: fixed / updated output processing scripts

--- hybrid-clingo/utils/make-readable.sh
+++ hybrid-clingo/utils/make-readable.sh
@@ -2,4 +2,4 @@
 
 # adds new lines to the output of clingo-lpx to improve readability
 
-sed -E 's|) |)\n|g; s|([0-9]) |\1\n|g; s|([-+*])e |\1e\n|g; s|=e |=e\n|g'
+sed -E 's|\) |\)\n|g; s|([0-9]) |\1\n|g; s|([-+*])e |\1e\n|g; s|=e |=e\n|g'
--- hybrid-clingo/utils/replace_assignments.sh
+++ hybrid-clingo/utils/replace_assignments.sh
@@ -1,6 +1,16 @@
 #!/bin/bash
 
 # takes replaces terms/atoms in the Answer by their corresponding interpreted values from the Assignment
+# if there is no matching term/atom in the Answer for an Assignment, then it will stay as leftovers (will still be printed in the Assignment section)
+
+
+LEFTOVERS=true
+if [ $# -eq 1 ]; then
+    LEFTOVERS=false
+# else
+#     echo "Usage: $0 [.*]"
+#     exit 1
+fi
 
 MODE="Skip"
 ANSW_HEAD=""
@@ -22,13 +32,34 @@ while IFS= read LINE
                 elif [ $MODE = "Assignment" ]; then
                     assignment_AsLines=$(echo "$LINE" | sed 's|\([0-9]\) |\1\n|g')
                     newAnswer_AsLines=$(echo "$answer_AsLines")
+                    leftoverAssignment=""
                     for assignment in $(echo "$assignment_AsLines"); do
                         name=$(echo "$assignment" | cut -d = -f 1)
                         value=$(echo "$assignment" | cut -d = -f 2)
-                        newAnswer_AsLines=$(echo "$newAnswer_AsLines" | sed "s|$name|$value|")
+                        if $LEFTOVERS; then 
+                            # if this assignment is in the answer then replace it and dont include it in the assignment section, otherwise keep it there
+                            if echo "$newAnswer_AsLines" | grep -q "$name"; then
+                                newAnswer_AsLines=$(echo "$newAnswer_AsLines" | sed "s|$name|$value|g")
+                            else
+                                leftoverAssignment=$(echo $leftoverAssignment $assignment)
+                            fi
+                        else
+                            newAnswer_AsLines=$(echo "$newAnswer_AsLines" | sed "s|$name|$value|g")
+                            leftoverAssignment=$(echo $leftoverAssignment $assignment)
+                        fi
                     done
                     newAnswer=$(echo $newAnswer_AsLines)
                     echo "$newAnswer"
+                    if $LEFTOVERS; then 
+                        # only print the assignment section if there are some leftover assignments 
+                        if echo "$leftoverAssignment" | grep -q "="; then
+                            echo "Assignment: [leftovers]"
+                            echo "$leftoverAssignment"
+                        fi
+                    else
+                            echo "Assignment:"
+                            echo "$leftoverAssignment"
+                    fi
                     MODE="Skip"
                 else
                     echo "$LINE"
